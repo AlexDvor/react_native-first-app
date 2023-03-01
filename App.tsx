8a649d6a49f4a4281a144c562c147df2ea38c3d8
Alex <alexmatvichuk@gmail.com>
2023-03-01 19:42:38 +0100

    add google font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
+import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		'OpenSans-Regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+	})
+
+	if (!fontsLoaded) {
+		return null
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text>Hello! </Text>
+			<Text style={styles.font}>Hellokkk!</Text>
 			<StatusBar style="auto" />
 		</View>
 	)
@@ -16,5 +25,11 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	font: {
+		fontSize: 20,
+		color: '#37E39F',
+		// fontFamily: 'OpenSans-Regular',
+		// fontWeight: '700',
 	},
 })
