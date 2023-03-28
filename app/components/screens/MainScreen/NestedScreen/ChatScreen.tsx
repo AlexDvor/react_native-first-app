5d8d62836abeb7b5bd1b90d1bd413d55f0ce81c1
Alex <alexmatvichuk@gmail.com>
2023-03-28 20:49:43 +0200

    fix chat Screen

--- app/components/screens/MainScreen/NestedScreen/ChatScreen.tsx
+++ app/components/screens/MainScreen/NestedScreen/ChatScreen.tsx
@@ -1,7 +1,6 @@
-import Ionicons from '@expo/vector-icons/Fontisto'
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons'
 import React, { FC, useCallback, useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StatusBar, StyleSheet, View } from 'react-native'
 import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat'
 import { ChatProps } from '~interfaces/message.navigation.types'
 
@@ -11,7 +10,17 @@ import { ChatProps } from '~interfaces/message.navigation.types'
 const messageData = [
 	{
 		_id: 1,
-		text: 'Hello developer',
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 2,
+		text: 'Your message',
 		createdAt: new Date(),
 		user: {
 			_id: 2,
@@ -20,8 +29,8 @@ const messageData = [
 		},
 	},
 	{
-		_id: 2,
-		text: 'Hello world',
+		_id: 3,
+		text: 'My message',
 		createdAt: new Date(),
 		user: {
 			_id: 1,
@@ -29,18 +38,177 @@ const messageData = [
 			avatar: 'https://placeimg.com/140/140/any',
 		},
 	},
+	{
+		_id: 4,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 5,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 6,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 7,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 8,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 9,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 10,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 11,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 12,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 13,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 14,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 15,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 16,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 17,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 18,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 19,
+		text: 'My message',
+		createdAt: new Date(),
+		user: {
+			_id: 1,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
+	{
+		_id: 20,
+		text: 'Your message',
+		createdAt: new Date(),
+		user: {
+			_id: 2,
+			name: 'React Native',
+			avatar: 'https://placeimg.com/140/140/any',
+		},
+	},
 ]
-
-// interface TChatState {
-// 	_id: number
-// 	text: string
-// 	createdAt: {}
-// 	user: {
-// 		_id: number
-// 		name: string
-// 		avatar: string
-// 	}
-// }
 
 export const ChatScreen: FC<ChatProps> = ({ route }) => {
 	// const userData = route.params.user
@@ -87,36 +255,30 @@ export const ChatScreen: FC<ChatProps> = ({ route }) => {
 	}
 
 	const scrollToBottomComponent = () => {
-		return (
-			<Ionicons
-				name="heart-alt"
-				size={22}
-				color={'#333'}
-				style={{ marginBottom: 5, marginRight: 5 }}
-			/>
-		)
+		return <AntDesign name="down" size={22} color={'#333'} style={{}} />
 	}
 
 	return (
-		<GiftedChat
-			messages={messages}
-			onSend={(messages: any) => onSend(messages)}
-			user={{
-				_id: 1,
-			}}
-			renderBubble={renderBubble}
-			alwaysShowSend
-			renderSend={renderSend}
-			scrollToBottom
-			scrollToBottomComponent={scrollToBottomComponent}
-		/>
+		<View style={styles.container}>
+			<GiftedChat
+				messages={messages}
+				onSend={(messages: any) => onSend(messages)}
+				user={{
+					_id: 1,
+				}}
+				renderBubble={renderBubble}
+				alwaysShowSend
+				renderSend={renderSend}
+				scrollToBottom
+				scrollToBottomComponent={scrollToBottomComponent}
+			/>
+		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
+		marginTop: StatusBar.currentHeight && StatusBar.currentHeight,
 	},
 })
